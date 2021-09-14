edd5c2c6140d4046cb2c060e1331d1826e17591f
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2021-09-14 12:55:45 -0300

    user feedback

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        Question(q: "Four + Two is equal to Six", a: "True"),
+        Question(q: "Five - Three is greater than One", a: "True"),
+        Question(q: "Three + Eight is less than Ten", a: "False")
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        questionLabel.text = quiz[questionNumber].text
+        progressBar.progress = 0
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        
+        if userAnswer == quiz[questionNumber].answer {
+            sender.backgroundColor = UIColor.green
+        }else{
+            sender.backgroundColor = UIColor.red
+        }
+        
+        if questionNumber < quiz.count-1{
+            questionNumber += 1
+        }else{
+            questionNumber = 0
+            progressBar.progress = 0
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber].text
+        progressBar.progress += 1.0/Float(quiz.count)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { // Change `2.0` to the desired number of seconds.
+            self.trueButton.backgroundColor = UIColor.clear
+            self.falseButton.backgroundColor = UIColor.clear
+        }
+    }
 }
 
